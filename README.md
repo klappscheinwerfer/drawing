04efad77786ec99036e150bd715110853eb0ab72
klappscheinwerfer <toms.grants@inbox.lv>
2022-09-19 11:31:16 +0300

    Add OpenGL

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-## Requirements
-
 ## Installation
 * Clone repository `git clone https://github.com/klappscheinwerfer/drawing.git`
 * Build
 	* `mkdir build`
-    * `cd build`
-    * `cmake ..`
-    * `make`
+	* `cd build`
+	* `cmake ..`
+	* `make`
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <GL/glut.h>
+#include <GL/glu.h>
 
-int main() {
-	std::cout << "Hello world\n";
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_LINES);
+	glVertex2i(100,150);
+	glVertex2i(200,300);
+	glEnd();
+
+	glFlush();
+}
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,500,0,500);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Hello");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
 	return 0;
 }
